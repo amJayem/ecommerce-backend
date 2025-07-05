334d8a1fea3c004248c7e057976cc6c3bdcd37d2
amjayem <csejayem19@gmail.com>
2025-07-05 20:18:47 +0600

    update migration after removing the old user table

---
+++ prisma/migrations/20250705135959_update/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `address` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `phoneNumber` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "address",
+DROP COLUMN "firstName",
+DROP COLUMN "lastName",
+DROP COLUMN "phoneNumber",
+DROP COLUMN "role",
+DROP COLUMN "username",
+ALTER COLUMN "name" SET NOT NULL;
