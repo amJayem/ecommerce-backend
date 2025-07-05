5d6591887b99cf8d66de3c7dce84c3b991ab3a75
amjayem <csejayem19@gmail.com>
2025-07-05 18:25:08 +0600

    feat: auth module controller service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ProductModule } from './product/product.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, UsersModule],
+  imports: [ProductModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
