eca57e5c1d8fbf1e63ff5707c6785c567bf3cdcf
amjayem <csejayem19@gmail.com>
2025-07-16 01:32:40 +0600

    install: cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ✅ Enable cookie parsing for incoming requests
+  app.use(cookieParser);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,4 +21,4 @@ async function bootstrap() {
   });
   await app.listen(process.env.PORT ?? 3333);
 }
-bootstrap();
+void bootstrap();
