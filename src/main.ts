492c6aaf76b14359b1f27e1f0226895fab68cadf
amjayem <csejayem19@gmail.com>
2025-07-16 23:42:08 +0600

    chore: port and prefix declare into variable; & fix: call cookie as func

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,28 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+const port = process.env.PORT ?? 3333;
+const prefix = `/api/v1`;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // ✅ Enable cookie parsing for incoming requests
-  app.use(cookieParser);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  app.setGlobalPrefix(`api/v1`);
+  app.setGlobalPrefix(prefix);
   app.enableCors({
-    origin: ['http://localhost:3001'], // ✅ only allow your domain
+    origin: ['http://localhost:3000'], // ✅ only allow your domain
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(port, () => {
+    console.log(`✅ Server running on: http://localhost:${port}${prefix}`);
+  });
 }
 void bootstrap();
